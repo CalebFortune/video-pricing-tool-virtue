b90c8c025636364f4de721cfec2304e8d58c94c0
CalebFortune <136854121+CalebFortune@users.noreply.github.com>
2023-09-04 14:55:23 -0500

    Update script.js
    Signed-off-by: CalebFortune <136854121+CalebFortune@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,68 +1,92 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const videoSlider = document.getElementById('videoSlider');
-    const monthlyCostElement = document.getElementById('monthlyCostDisplay');
-    const annualCostElement = document.getElementById('annualCostDisplay');
-    const SEOCheckbox = document.getElementById('SEOAndSocialAudit');
-    const HostingCheckbox = document.getElementById('HostingAndDataManagement');
-    const ProofingCheckbox = document.getElementById('ProofingAndReEdits');
-    const StrategyCheckbox = document.getElementById('StrategyAndReporting');
-    const CopyCheckbox = document.getElementById('CopyAndScriptWriting');
+// Initialize Parse
+Parse.initialize("leK5sqZJbjoK4kFbo5jM1HKv2hfW24A98OsBR00z", "ErjjnF7tF4SM9seMZsjfA0P5xlYtdhYHhSDeXw6G");
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
-    function updatePrice() {
-        const baseVideoCost = 1640;
-        const quarterlyCost = 1000;
-        const yearlyHostingCost = 1000;
-        const yearlySEOCost = 500;
-        const proofingCostPerVideo = 200;
-        const strategyCostPerVideo = 300;
-        const copyCostPerVideo = 250;
-        const markup = 0.30;
+const videoSlider = document.getElementById('videoSlider');
+const monthlyCostElement = document.getElementById('monthlyCostDisplay');
+const annualCostElement = document.getElementById('annualCostDisplay');
+const SEOCheckbox = document.getElementById('SEOAndSocialAudit');
+const HostingCheckbox = document.getElementById('HostingAndDataManagement');
+const ProofingCheckbox = document.getElementById('ProofingAndReEdits');
+const StrategyCheckbox = document.getElementById('StrategyAndReporting');
+const CopyCheckbox = document.getElementById('CopyAndScriptWriting');
+const userInfoSection = document.getElementById('userInfoSection');
 
-        let videosPerMonth = parseInt(videoSlider.value);
-        let totalMonthlyCost = videosPerMonth * baseVideoCost;
-        let totalYearlyCost = totalMonthlyCost * 12 + quarterlyCost * 4;
+function updatePrice() {
+    const baseVideoCost = 1640;
+    const quarterlyCost = 1000;
+    const yearlyHostingCost = 1000;
+    const yearlySEOCost = 500;
+    const proofingCostPerVideo = 200;
+    const strategyCostPerVideo = 300;
+    const copyCostPerVideo = 250;
+    const markup = 0.30;
 
-        if (!SEOCheckbox.checked) {
-            totalYearlyCost -= yearlySEOCost;
-        }
-        if (!HostingCheckbox.checked) {
-            totalYearlyCost -= yearlyHostingCost;
-        }
-        if (!ProofingCheckbox.checked) {
-            totalYearlyCost -= proofingCostPerVideo * videosPerMonth * 12;
-        }
-        if (!StrategyCheckbox.checked) {
-            totalYearlyCost -= strategyCostPerVideo * videosPerMonth * 12;
-        }
-        if (!CopyCheckbox.checked) {
-            totalYearlyCost -= copyCostPerVideo * videosPerMonth * 12;
-        }
+    let videosPerMonth = parseInt(videoSlider.value);
+    let totalMonthlyCost = videosPerMonth * baseVideoCost;
+    let totalYearlyCost = totalMonthlyCost * 12 + quarterlyCost * 4;
 
-        totalMonthlyCost = totalYearlyCost / 12;
-
-        // Apply markup
-        totalMonthlyCost += totalMonthlyCost * markup;
-        totalYearlyCost += totalYearlyCost * markup;
-
-        monthlyCostElement.textContent = `$${Math.round(totalMonthlyCost)}`;
-        annualCostElement.textContent = `$${Math.round(totalYearlyCost)}`;
+    if (!SEOCheckbox.checked) {
+        totalYearlyCost -= yearlySEOCost;
+    }
+    if (!HostingCheckbox.checked) {
+        totalYearlyCost -= yearlyHostingCost;
+    }
+    if (!ProofingCheckbox.checked) {
+        totalYearlyCost -= proofingCostPerVideo * videosPerMonth * 12;
+    }
+    if (!StrategyCheckbox.checked) {
+        totalYearlyCost -= strategyCostPerVideo * videosPerMonth * 12;
+    }
+    if (!CopyCheckbox.checked) {
+        totalYearlyCost -= copyCostPerVideo * videosPerMonth * 12;
     }
 
-    videoSlider.addEventListener('input', function() {
-        document.getElementById('sliderValue').textContent = videoSlider.value;
-        updatePrice();
-    });
+    totalMonthlyCost = totalYearlyCost / 12;
 
-    SEOCheckbox.addEventListener('change', updatePrice);
-    HostingCheckbox.addEventListener('change', updatePrice);
-    ProofingCheckbox.addEventListener('change', updatePrice);
-    StrategyCheckbox.addEventListener('change', updatePrice);
-    CopyCheckbox.addEventListener('change', updatePrice);
+    totalMonthlyCost += totalMonthlyCost * markup;
+    totalYearlyCost += totalYearlyCost * markup;
 
+    monthlyCostElement.textContent = `$${totalMonthlyCost.toFixed(2)}`;
+    annualCostElement.textContent = `$${totalYearlyCost.toFixed(2)}`;
+}
+
+videoSlider.addEventListener('input', function() {
+    document.getElementById('sliderValue').textContent = videoSlider.value;
     updatePrice();
+    showUserInfoSection();
 });
 
-        // Initialize Parse
-        Parse.initialize("leK5sqZJbjoK4kFbo5jM1HKv2hfW24A98OsBR00z", "ErjjnF7tF4SM9seMZsjfA0P5xlYtdhYHhSDeXw6G");
-        Parse.serverURL = 'https://parseapi.back4app.com/';
+[SEOCheckbox, HostingCheckbox, ProofingCheckbox, StrategyCheckbox, CopyCheckbox].forEach(checkbox => {
+    checkbox.addEventListener('change', updatePrice);
+    checkbox.addEventListener('change', showUserInfoSection);
+});
 
+function showUserInfoSection() {
+    userInfoSection.style.display = 'block';
+}
+
+document.getElementById('submitInfo').addEventListener('click', async function() {
+    const UserSelections = Parse.Object.extend("UserSelections");
+    const userSelection = new UserSelections();
+
+    userSelection.set("videosPerMonth", parseInt(videoSlider.value));
+    userSelection.set("SEOAndSocialAudit", SEOCheckbox.checked);
+    userSelection.set("HostingAndDataManagement", HostingCheckbox.checked);
+    userSelection.set("ProofingAndReEdits", ProofingCheckbox.checked);
+    userSelection.set("StrategyAndReporting", StrategyCheckbox.checked);
+    userSelection.set("CopyAndScriptWriting", CopyCheckbox.checked);
+    userSelection.set("userName", document.getElementById('userName').value);
+    userSelection.set("userEmail", document.getElementById('userEmail').value);
+    userSelection.set("companyName", document.getElementById('companyName').value);
+    userSelection.set("userIntent", document.getElementById('userIntent').value);
+
+    try {
+        await userSelection.save();
+        alert('Your selections and information have been saved!');
+    } catch (error) {
+        alert('Failed to save your selections: ' + error.message);
+    }
+});
+
+updatePrice();
