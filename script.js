f11692bd5ec8d6b276a471f67a2a31a99f63b28b
CalebFortune <136854121+CalebFortune@users.noreply.github.com>
2023-09-05 11:01:53 -0500

    Update script.js
    Signed-off-by: CalebFortune <136854121+CalebFortune@users.noreply.github.com>

--- script.js
+++ script.js
@@ -13,20 +13,11 @@ const CopyCheckbox = document.getElementById('CopyAndScriptWriting');
 const userInfoSection = document.getElementById('userInfoSection');
 
 function updatePrice() {
-    const firstVideoCost = 1640;
-    const subsequentVideoCost = 1340;
-    let videosPerMonth = parseInt(videoSlider.value);
-    let totalMonthlyCost = firstVideoCost + (subsequentVideoCost * (videosPerMonth - 1));
-    let totalYearlyCost = totalMonthlyCost * 12;
-
-    monthlyCostElement.textContent = `$${totalMonthlyCost.toFixed(2)}`;
-    annualCostElement.textContent = `$${totalYearlyCost.toFixed(2)}`;
+    // ... (rest of the pricing logic)
 }
 
 videoSlider.addEventListener('input', function() {
-    document.getElementById('sliderValue').textContent = videoSlider.value;
-    updatePrice();
-    showUserInfoSection();
+    // ... (rest of the slider logic)
 });
 
 [SEOCheckbox, HostingCheckbox, ProofingCheckbox, StrategyCheckbox, CopyCheckbox].forEach(checkbox => {
@@ -39,22 +30,20 @@ function showUserInfoSection() {
 }
 
 document.getElementById('submitInfo').addEventListener('click', async function() {
-    const data = {
-        videosPerMonth: parseInt(videoSlider.value),
-        SEOAndSocialAudit: SEOCheckbox.checked,
-        HostingAndDataManagement: HostingCheckbox.checked,
-        ProofingAndReEdits: ProofingCheckbox.checked,
-        StrategyAndReporting: StrategyCheckbox.checked,
-        CopyAndScriptWriting: CopyCheckbox.checked,
-        userName: document.getElementById('userName').value,
-        userEmail: document.getElementById('userEmail').value,
-        companyName: document.getElementById('companyName').value,
-        userIntent: document.getElementById('userIntent').value
-    };
-
     try {
-        const result = await Parse.Cloud.run('saveUserSelections', data);
-        if (result && result.success) {
+        const response = await Parse.Cloud.run('saveUserSelections', {
+            videosPerMonth: parseInt(videoSlider.value),
+            SEOAndSocialAudit: SEOCheckbox.checked,
+            HostingAndDataManagement: HostingCheckbox.checked,
+            ProofingAndReEdits: ProofingCheckbox.checked,
+            StrategyAndReporting: StrategyCheckbox.checked,
+            CopyAndScriptWriting: CopyCheckbox.checked,
+            userName: document.getElementById('userName').value,
+            userEmail: document.getElementById('userEmail').value,
+            companyName: document.getElementById('companyName').value,
+            userIntent: document.getElementById('userIntent').value
+        });
+        if (response && response.success) {
             alert('Your selections and information have been saved!');
         } else {
             alert('Failed to save your selections.');
