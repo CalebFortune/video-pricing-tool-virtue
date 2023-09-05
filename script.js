e5b2c0377ad40cfc8aea356858c7e2f7cf51aef1
CalebFortune <136854121+CalebFortune@users.noreply.github.com>
2023-09-05 11:35:08 -0500

    Update script.js
    Signed-off-by: CalebFortune <136854121+CalebFortune@users.noreply.github.com>

--- script.js
+++ script.js
@@ -15,16 +15,45 @@ const userInfoSection = document.getElementById('userInfoSection');
 function updatePrice() {
     const firstVideoCost = 1640;
     const subsequentVideoCost = 1340;
+    const quarterlyCost = 1000;
+    const yearlyHostingCost = 1000;
+    const yearlySEOCost = 500;
+    const proofingCostPerVideo = 200;
+    const strategyCostPerVideo = 300;
+    const copyCostPerVideo = 250;
+    const markup = 0.30;
+
     let videosPerMonth = parseInt(videoSlider.value);
-    let totalMonthlyCost = firstVideoCost + (subsequentVideoCost * (videosPerMonth - 1));
-    let totalYearlyCost = totalMonthlyCost * 12;
+    let totalMonthlyCost = (videosPerMonth === 1) ? firstVideoCost : firstVideoCost + (subsequentVideoCost * (videosPerMonth - 1));
+    let totalYearlyCost = totalMonthlyCost * 12 + quarterlyCost * 4;
+
+    if (!SEOCheckbox.checked) {
+        totalYearlyCost -= yearlySEOCost;
+    }
+    if (!HostingCheckbox.checked) {
+        totalYearlyCost -= yearlyHostingCost;
+    }
+    if (!ProofingCheckbox.checked) {
+        totalYearlyCost -= proofingCostPerVideo * videosPerMonth * 12;
+    }
+    if (!StrategyCheckbox.checked) {
+        totalYearlyCost -= strategyCostPerVideo * videosPerMonth * 12;
+    }
+    if (!CopyCheckbox.checked) {
+        totalYearlyCost -= copyCostPerVideo * videosPerMonth * 12;
+    }
+
+    totalMonthlyCost = totalYearlyCost / 12;
+
+    totalMonthlyCost += totalMonthlyCost * markup;
+    totalYearlyCost += totalYearlyCost * markup;
 
     monthlyCostElement.textContent = `$${totalMonthlyCost.toFixed(2)}`;
     annualCostElement.textContent = `$${totalYearlyCost.toFixed(2)}`;
 }
 
 videoSlider.addEventListener('input', function() {
- document.getElementById('sliderValue').textContent = videoSlider.value;
+    document.getElementById('sliderValue').textContent = videoSlider.value;
     updatePrice();
     showUserInfoSection();
 });
@@ -39,24 +68,23 @@ function showUserInfoSection() {
 }
 
 document.getElementById('submitInfo').addEventListener('click', async function() {
+    const UserSelections = Parse.Object.extend("UserSelections");
+    const userSelection = new UserSelections();
+
+    userSelection.set("videosPerMonth", parseInt(videoSlider.value));
+    userSelection.set("SEOAndSocialAudit", SEOCheckbox.checked);
+    userSelection.set("HostingAndDataManagement", HostingCheckbox.checked);
+    userSelection.set("ProofingAndReEdits", ProofingCheckbox.checked);
+    userSelection.set("StrategyAndReporting", StrategyCheckbox.checked);
+    userSelection.set("CopyAndScriptWriting", CopyCheckbox.checked);
+    userSelection.set("userName", document.getElementById('userName').value);
+    userSelection.set("userEmail", document.getElementById('userEmail').value);
+    userSelection.set("companyName", document.getElementById('companyName').value);
+    userSelection.set("userIntent", document.getElementById('userIntent').value);
+
     try {
-        const response = await Parse.Cloud.run('saveUserSelections', {
-            videosPerMonth: parseInt(videoSlider.value),
-            SEOAndSocialAudit: SEOCheckbox.checked,
-            HostingAndDataManagement: HostingCheckbox.checked,
-            ProofingAndReEdits: ProofingCheckbox.checked,
-            StrategyAndReporting: StrategyCheckbox.checked,
-            CopyAndScriptWriting: CopyCheckbox.checked,
-            userName: document.getElementById('userName').value,
-            userEmail: document.getElementById('userEmail').value,
-            companyName: document.getElementById('companyName').value,
-            userIntent: document.getElementById('userIntent').value
-        });
-        if (response && response.success) {
-            alert('Your selections and information have been saved!');
-        } else {
-            alert('Failed to save your selections.');
-        }
+        await userSelection.save();
+        alert('Your selections and information have been saved!');
     } catch (error) {
         alert('Failed to save your selections: ' + error.message);
     }
