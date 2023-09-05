a5ed5c285ce2effbfd241ae40a4ef967f98df974
CalebFortune <136854121+CalebFortune@users.noreply.github.com>
2023-09-05 13:45:57 -0500

    Update script.js
    Signed-off-by: CalebFortune <136854121+CalebFortune@users.noreply.github.com>

--- script.js
+++ script.js
@@ -10,39 +10,61 @@ const HostingCheckbox = document.getElementById('HostingAndDataManagement');
 const ProofingCheckbox = document.getElementById('ProofingAndReEdits');
 const StrategyCheckbox = document.getElementById('StrategyAndReporting');
 const CopyCheckbox = document.getElementById('CopyAndScriptWriting');
+const ROIReportingCheckbox = document.getElementById('ROIReporting');
+const LicensedMusicCheckbox = document.getElementById('LicensedMusicAndStock');
+const EmailNewsletterCheckbox = document.getElementById('EmailNewsletterCopy');
+const CustomAnimationCheckbox = document.getElementById('CustomAnimation');
+const VideoAdManagementCheckbox = document.getElementById('VideoAdManagement');
 const userInfoSection = document.getElementById('userInfoSection');
 
 function updatePrice() {
-    const firstVideoCost = 1640;
-    const subsequentVideoCost = 1340;
+    const firstVideoCost = 1650;
+    const subsequentVideoCost = 1150;
     const quarterlyCost = 1000;
-    const yearlyHostingCost = 1000;
+    const yearlyHostingCost = 850;
     const yearlySEOCost = 500;
-    const proofingCostPerVideo = 200;
-    const strategyCostPerVideo = 300;
+    const proofingCostPerVideo = 100;
+    const firstStrategyCostPerVideo = 250;
+    const subsequentStrategyCostPerVideo = 100;
     const copyCostPerVideo = 250;
+    const quarterlyROICost = 300;
+    const yearlyMusicCost = 240;
+    const quarterlyEmailNewsletterCost = 1200;
+    const quarterlyCustomAnimationCost = 2500;
+    const quarterlyVideoAdManagementCost = 5400;
     const markup = 0.30;
 
     let videosPerMonth = parseInt(videoSlider.value);
     let totalMonthlyCost = (videosPerMonth === 1) ? firstVideoCost : firstVideoCost + (subsequentVideoCost * (videosPerMonth - 1));
+    let totalYearlyCost = totalMonthlyCost * 12 + quarterlyCost * 4;
 
-    if (SEOCheckbox.checked) {
-        totalMonthlyCost += yearlySEOCost / 12;
+    if (!SEOCheckbox.checked) {
+        totalYearlyCost -= yearlySEOCost;
     }
-    if (HostingCheckbox.checked) {
-        totalMonthlyCost += yearlyHostingCost / 12;
+    if (!HostingCheckbox.checked) {
+        totalYearlyCost -= yearlyHostingCost;
     }
-    if (ProofingCheckbox.checked) {
-        totalMonthlyCost += proofingCostPerVideo * videosPerMonth;
+    totalYearlyCost += proofingCostPerVideo * videosPerMonth * 12;
+    totalYearlyCost += (videosPerMonth === 1) ? firstStrategyCostPerVideo : firstStrategyCostPerVideo + (subsequentStrategyCostPerVideo * (videosPerMonth - 1)) * 12;
+    totalYearlyCost += copyCostPerVideo * videosPerMonth * 12;
+
+    if (ROIReportingCheckbox.checked) {
+        totalYearlyCost += quarterlyROICost * 4;
     }
-    if (StrategyCheckbox.checked) {
-        totalMonthlyCost += strategyCostPerVideo * videosPerMonth;
+    if (LicensedMusicCheckbox.checked) {
+        totalYearlyCost += yearlyMusicCost;
     }
-    if (CopyCheckbox.checked) {
-        totalMonthlyCost += copyCostPerVideo * videosPerMonth;
+    if (EmailNewsletterCheckbox.checked) {
+        totalYearlyCost += quarterlyEmailNewsletterCost * 4;
+    }
+    if (CustomAnimationCheckbox.checked) {
+        totalYearlyCost += quarterlyCustomAnimationCost * 4;
+    }
+    if (VideoAdManagementCheckbox.checked) {
+        totalYearlyCost += quarterlyVideoAdManagementCost * 4;
     }
 
-    let totalYearlyCost = (totalMonthlyCost * 12) + (quarterlyCost * 4);
+    totalMonthlyCost = totalYearlyCost / 12;
 
     totalMonthlyCost += totalMonthlyCost * markup;
     totalYearlyCost += totalYearlyCost * markup;
@@ -51,14 +73,13 @@ function updatePrice() {
     annualCostElement.textContent = `$${totalYearlyCost.toFixed(2)}`;
 }
 
-
 videoSlider.addEventListener('input', function() {
     document.getElementById('sliderValue').textContent = videoSlider.value;
     updatePrice();
     showUserInfoSection();
 });
 
-[SEOCheckbox, HostingCheckbox, ProofingCheckbox, StrategyCheckbox, CopyCheckbox].forEach(checkbox => {
+[SEOCheckbox, HostingCheckbox, ProofingCheckbox, StrategyCheckbox, CopyCheckbox, ROIReportingCheckbox, LicensedMusicCheckbox, EmailNewsletterCheckbox, CustomAnimationCheckbox, VideoAdManagementCheckbox].forEach(checkbox => {
     checkbox.addEventListener('change', updatePrice);
     checkbox.addEventListener('change', showUserInfoSection);
 });
@@ -77,6 +98,11 @@ document.getElementById('submitInfo').addEventListener('click', async function()
     userSelection.set("ProofingAndReEdits", ProofingCheckbox.checked);
     userSelection.set("StrategyAndReporting", StrategyCheckbox.checked);
     userSelection.set("CopyAndScriptWriting", CopyCheckbox.checked);
+    userSelection.set("ROIReporting", ROIReportingCheckbox.checked);
+    userSelection.set("LicensedMusicAndStock", LicensedMusicCheckbox.checked);
+    userSelection.set("EmailNewsletterCopy", EmailNewsletterCheckbox.checked);
+    userSelection.set("CustomAnimation", CustomAnimationCheckbox.checked);
+    userSelection.set("VideoAdManagement", VideoAdManagementCheckbox.checked);
     userSelection.set("userName", document.getElementById('userName').value);
     userSelection.set("userEmail", document.getElementById('userEmail').value);
     userSelection.set("companyName", document.getElementById('companyName').value);
